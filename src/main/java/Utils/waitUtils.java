8d1d71b79e7b0c4b3d918e7e98714ad69e25abae
gondhalekiran <124554646+gondhalekiran@users.noreply.github.com>
2025-01-27 11:50:08 +0530

    Update waitUtils.java

--- src/main/java/Utils/waitUtils.java
+++ src/main/java/Utils/waitUtils.java
@@ -1,5 +1,5 @@
 package Utils;
 
 public class waitUtils {
-
+public static void waitForAjax(WebDriver driver) {     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));     wait.until(driver1 -> {       JavascriptExecutor js = (JavascriptExecutor) driver1;       //Return true if no active AJAX requests are found       return (Boolean) js.executeScript("return jQuery.active == 0");     });   }
 }
